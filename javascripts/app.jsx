80ed51b2fee01b95d22c8d126644be4c2fa4f36b
Lukasz Sagol <lukasz@sagol.pl>
2014-06-23 00:11:09 +0100

    Rendering of nested lists

--- javascripts/app.jsx
+++ javascripts/app.jsx
@@ -6,8 +6,11 @@ window.data = {
   tree: [{
     name: 'Lists TODO',
     children: [{
-      name: '[ ] Rendering of nested lists',
-      children: []
+      name: '[x] Rendering of nested lists',
+      children: [{
+        name: '[x] Just to verify multiple levels of nesting',
+        children: []
+      }]
     },{
       name: '[ ] Adding new items',
       children: []
@@ -27,16 +30,49 @@ window.data = {
   }]
 }
 
+var Item = React.createClass({
+  render: function() {
+    return (
+      <div className="list-item">
+        <div className="list-item--name">
+          {this.props.name}
+        </div>
+        <div className="list-item--children">
+          <Tree nodes={this.props.children} />
+        </div>
+      </div>
+    )
+  }
+})
+
+var Tree = React.createClass({
+  render: function() {
+    var items = this.props.nodes.map(function(item) {
+      return (<Item name={item.name} children={item.children} />)
+    })
+
+    return (
+      <div className="list-tree">
+        {items}
+      </div>
+    )
+  }
+})
+
 var App = React.createClass({
   getInitialState: function() {
     return ({data: {}})
   },
+
   componentWillMount: function() {
     this.setState({data: window.data})
   },
+
   render: function() {
     return (
-      <div>Hello, components.</div>
+      <div className="list">
+        <Tree nodes={this.state.data.tree} />
+      </div>
     )
   }
 })
